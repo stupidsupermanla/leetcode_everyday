59822967d982dba008741bba87e4dbb964a832d4
Administrator <hudihoho@163.com>
2021-02-11 18:02:00 +0800

    2021-2-11 每日一题 数据流中第 k 大元素的类：保留K位的单调队列

---
+++ src/main/java/leetcode/leetcode/editor/cn/P703KthLargestElementInAStream.java
@@ -0,0 +1,96 @@
+//设计一个找到数据流中第 k 大元素的类（class）。注意是排序后的第 k 大元素，不是第 k 个不同的元素。 
+//
+// 请实现 KthLargest 类： 
+//
+// 
+// KthLargest(int k, int[] nums) 使用整数 k 和整数流 nums 初始化对象。 
+// int add(int val) 将 val 插入数据流 nums 后，返回当前数据流中第 k 大的元素。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//["KthLargest", "add", "add", "add", "add", "add"]
+//[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+//输出：
+//[null, 4, 5, 5, 8, 8]
+//
+//解释：
+//KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+//kthLargest.add(3);   // return 4
+//kthLargest.add(5);   // return 5
+//kthLargest.add(10);  // return 5
+//kthLargest.add(9);   // return 8
+//kthLargest.add(4);   // return 8
+// 
+//
+// 
+//提示：
+//
+// 
+// 1 <= k <= 104 
+// 0 <= nums.length <= 104 
+// -104 <= nums[i] <= 104 
+// -104 <= val <= 104 
+// 最多调用 add 方法 104 次 
+// 题目数据保证，在查找第 k 大元素时，数组中至少有 k 个元素 
+// 
+// Related Topics 堆 设计 
+// 👍 210 👎 0
+
+
+package leetcode.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * Java：数据流中的第 K 大元素
+ */
+public class P703KthLargestElementInAStream {
+    public static void main(String[] args) {
+//        KthLargest solution = new P703KthLargestElementInAStream().new Solution();
+        // TO TEST
+        KthLargest kthLargest =new KthLargest(3,new int[]{4,5,8,2});
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+        System.out.println(kthLargest.add(9));
+        System.out.println(kthLargest.add(4));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+  static   class KthLargest {
+        private int k;
+        PriorityQueue<Integer> queue;
+
+        public KthLargest(int k, int[] nums) {
+            this.k = k;
+            this.queue = new PriorityQueue<>(Comparator.comparingInt(a->a));
+            for (int num : nums) {
+                queue.add(num);
+            }
+        }
+
+        public int add(int val) {
+            queue.add(val);
+            while (queue.size() > k) {     // 维持 队列的大小与kk值相等
+                queue.poll();    //弹出元素
+            }
+            return queue.peek();
+        }
+    }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
