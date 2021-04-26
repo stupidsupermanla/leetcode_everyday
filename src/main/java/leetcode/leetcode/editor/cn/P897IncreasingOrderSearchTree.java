2bd544506e17bd91db61337ef81330a25f4d0cab
hudi <hudi@changingedu.com>
2021-04-26 17:02:18 +0800

    2021-04-25 每日一题：中序遍历构建新树 2021-04-26 每日一题：二分法在预测范围内找到符合条件最小值

---
+++ src/main/java/leetcode/leetcode/editor/cn/P897IncreasingOrderSearchTree.java
@@ -0,0 +1,90 @@
+//给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+//输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [5,1,7]
+//输出：[1,null,5,null,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的取值范围是 [1, 100] 
+// 0 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 递归 
+// 👍 198 👎 0
+
+
+package leetcode.leetcode.editor.cn;
+//Java：递增顺序搜索树
+public class P897IncreasingOrderSearchTree{
+    public static void main(String[] args) {
+        Solution solution = new P897IncreasingOrderSearchTree().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode head = null;
+    TreeNode temp = null;
+    public TreeNode increasingBST(TreeNode root) {
+        mid(root);
+        return head;
+    }
+
+    private void mid(TreeNode root){
+        if (root==null){
+            return ;
+        }
+        mid(root.left);
+        if (head == null){
+            head=new TreeNode(root.val);
+            temp = head;
+        }else {
+            temp.right = new TreeNode(root.val);
+            temp = temp.right;
+        }
+        mid(root.right);
+    }
+}
+}
+//leetcode submit region end(Prohibit modification and deletion)
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
